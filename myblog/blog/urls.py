59f98e8659599e59feed2adb90667402aa8313a4
Areen Anshula <areenanshula2003@gmail.com>
2025-06-10 10:36:37 +0530

    basic structure

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -3,8 +3,6 @@ from . import  views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('places/', views.places, name='places'),
-    path('gallery/', views.gallery, name='gallery'),
-    path('blog/', views.blog, name='blog'),
-    path('signin/', views.signIn, name='signIn')
+    path('signin/', views.signIn, name='signIn'),
+    path('rooms/', views.rooms, name='rooms'),
 ]
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -8,13 +8,10 @@ def post_list(request):
     return render(request, 'blog/home.html', {'posts': posts})
 
 def places(request):
-    return render(request, 'blog/places.html')
-
-def gallery(request):
-    return render(request, 'blog/gallery.html')
-
-def blog(request):
-    return  render(request, 'blog/blog.html')
+    return render(request, 'blog/.html')
 
 def signIn(request):
     return render(request, 'blog/signIn.html')
+
+def rooms(request):
+    return render(request, 'blog/rooms.html')
